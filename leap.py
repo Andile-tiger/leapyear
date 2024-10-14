de9c31c1a93b1e0b1d476853bb2b3fbc330d2868
Andile-tiger <asejamitlwa@idm.ac.bw>
2024-10-14 12:16:21 +0200

    using python scripts

---
+++ leap.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        year = int(request.form['year'])
+        result = is_leap_year(year)
+        if result:
+            result = f"{year} is a leap year."
+        else:
+            result = f"{year} is not a leap year."
+        return render_template('index.html', result=result)
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
