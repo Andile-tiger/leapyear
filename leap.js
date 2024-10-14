94f9730dc42847b55c6623ac8401f25c3062c5f1
Andile-tiger <asejamitlwa@idm.ac.bw>
2024-10-14 13:51:31 +0200

    changes in leap.js

--- leap.js
+++ leap.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-function checkLeapYear() {
-    var year = document.getElementById("year").value;
-=======
+
 function checkLeapYear(){
     var year = document.getElementById("year").value;
->>>>>>> bd6bdb02c5a2de226107d7ae980d23a198363a47
 
     if (year % 4 == 0) {
         if (year % 100 != 0 || year % 400 == 0) {
